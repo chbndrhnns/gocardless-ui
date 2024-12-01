5fe6004d5298d9178ddc3c6ec4ce47f2355fe485
chbndrhnns <chbndrhnns@users.noreply.github.com>
2024-12-02 00:13:35 +0100

    Add anyio, streamline tests

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,4 +1,13 @@
+import pytest
+
 from server.services.sync_service import transform_transaction
+
+pytestmark = [pytest.mark.anyio]
+
+
+@pytest.fixture
+def anyio_backend():
+    return "asyncio"
 
 
 async def test_dkb():
@@ -7,54 +16,52 @@ async def test_dkb():
         "bookingDate": "2024-11-26",
         "valueDate": "2024-11-26",
         "transactionAmount": {"amount": "-700.0", "currency": "EUR"},
-        "creditorName": "ISA RUESCHEL",
-        "creditorAccount": {"iban": "DE92120300001036732731"},
-        "debtorName": "Johannes Rüschel",
-        "debtorAccount": {"iban": "DE91120300000017859299"},
+        "creditorName": "ISA",
+        "debtorName": "Johannes",
         "remittanceInformationUnstructured": "Lebensgeld fuer Isa",
         "purposeCode": "RINP",
-        "bankTransactionCode": "PMNT-ICDT-STDO",
-        "proprietaryBankTransactionCode": "NSTO+117+7000+997",
+        "bankTransactionCode": "",
+        "proprietaryBankTransactionCode": "",
         "internalTransactionId": "9799dd15f2a208fe09d1c8045c9805e5",
     }
 
     actual = await transform_transaction(tx, 12345)
 
     assert actual == {
-        "account_id": 12345,
         "amount": "-700.00",
+        "asset_id": 12345,
         "currency": "eur",
         "date": "2024-11-26",
         "external_id": "9799dd15f2a208fe09d1c8045c9805e5",
         "notes": "Lebensgeld fuer Isa",
-        "payee": "ISA RUESCHEL",
+        "payee": "ISA",
         "status": "uncleared",
     }
 
 
-def test_ing():
+async def test_ing():
     tx = {
         "bookingDate": "2024-11-22",
         "creditorName": "AUDIBLE GMBH",
-        "endToEndId": "541RZ71AMKXHFJRP                   ",
+        "endToEndId": "                   ",
         "internalTransactionId": "8d15ff4f8dc878f5fea3c878f44575ea",
         "proprietaryBankTransactionCode": "Lastschrifteinzug",
-        "remittanceInformationUnstructured": "mandatereference:0xjNei1bcEUn?iEBC24JpkhuZ88ATR,creditorid:DE31ZZZ00000563369,remittanceinformation:D01-6254502-7698230 "
+        "remittanceInformationUnstructured": "mandatereference:0xjNei1bcEUn?iEBC24JpkhuZ8,creditorid:DE31ZZZ00000563,remittanceinformation:D01-6254502-7698 "
         "Audible Gmbh "
-        "541RZ71AMKXHFJRP",
+                                             "541RZ71AMKXHF",
         "transactionAmount": {"amount": "-0.99", "currency": "EUR"},
         "transactionId": "000012247539913",
         "valueDate": "2024-11-22",
     }
-    actual = transform_transaction(tx, 12345)
+    actual = await transform_transaction(tx, 12345)
 
     assert actual == {
-        "account_id": 12345,
         "amount": "-0.99",
+        "asset_id": 12345,
         "currency": "eur",
         "date": "2024-11-22",
         "external_id": "8d15ff4f8dc878f5fea3c878f44575ea",
-        "notes": "D01-6254502-7698230 Audible Gmbh 541RZ71AMKXHFJRP",
+        "notes": "D01-6254502-7698 Audible Gmbh 541RZ71AMKXHF",
         "payee": "AUDIBLE GMBH",
         "status": "uncleared",
     }
